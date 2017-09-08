1e827135f2c129abd2d0daefba8483b1001e78db
Pacome BERNIER <pbernier@e1r5p19.42.fr>
2017-09-08 15:12:19 +0200

    [pbernier] - Set_link, fixed, version with print (and infinit loop).

--- srcs/init_link.c
+++ srcs/init_link.c
@@ -6,35 +6,71 @@
 /*   By: pbernier <pbernier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/07 03:37:58 by pbernier          #+#    #+#             */
-/*   Updated: 2017/09/08 14:22:38 by pbernier         ###   ########.fr       */
+/*   Updated: 2017/09/08 15:11:27 by pbernier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lem_in.h>
 
-void	realloc_link(t_lem *l, t_room **room)
+void	realloc_link(t_lem *l, t_room **room, t_room **obj)
 {
 	int		i;
 	t_room	**list;
 
-	i = -1;
+	i = 0;
 	if (!(list = (t_room **)malloc(sizeof(t_room *) * ((*room)->nb_link + 2) )))
 		error(l, MALLOC);
 	list[(*room)->nb_link + 1] = NULL;
 	++(*room)->nb_link;
-	while (((*room)->link[++i]))
+	list[i] = *obj;
+	while (((*room)->link[i]))
 		list[i + 1] = (*room)->link[i];
-	ft_memdel((void **)room);
-	room = list;
+
+	ft_memdel((void **)&(*room)->link);
+	(*room)->link = list;
 }
 
 void	link_list(t_lem *l, t_room **fst, t_room **sec)
 {
-	realloc_link(l, fst);
-	(*fst)->link[0] = (*sec);
-	realloc_link(l, sec);
-	(*sec)->link[0] = (*fst);
 
+	int	i = -1;
+
+	printf("  [%s] (avant)\n", (*fst)->name);
+	while (((*fst)->link[++i]))
+	{
+		printf("[%s] = ", (*fst)->link[i]->name);
+		printf("[%p]\n", (*fst)->link[i]);
+	}
+	printf("[%p]\n\n", (*fst)->link[i]);
+	i = -1;
+	realloc_link(l, fst, sec);
+	printf("  [%s] (aprés)\n", (*fst)->name);
+	while (((*fst)->link[++i]))
+	{
+		printf("[%s] = ", (*fst)->link[i]->name);
+		printf("[%p]\n", (*fst)->link[i]);
+	}
+	printf("[%p]\n\n", (*fst)->link[i]);
+	i = -1;
+	printf("  -------  \n\n");
+	printf("  [%s] (avant)\n", (*sec)->name);
+	while (((*sec)->link[++i]))
+	{
+		printf("[%s] = ", (*sec)->link[i]->name);
+		printf("[%p]\n", (*sec)->link[i]);
+	}
+	printf("[%p]\n\n", (*sec)->link[i]);
+	i = -1;
+	realloc_link(l, sec, fst);
+	printf("  [%s] (aprés)\n", (*sec)->name);
+	while (((*sec)->link[++i]))
+	{
+		printf("[%s] = ", (*sec)->link[i]->name);
+		printf("[%p]\n", (*sec)->link[i]);
+	}
+	printf("[%p]\n", (*sec)->link[i]);
+
+	while(1);
 }
 
 void	set_link(t_lem *l, char *name_check, int sw)
@@ -63,7 +99,6 @@ int		init_links(t_lem *l)
 	if ((l->p.line[0] == '#') || (!ft_strncmp(l->p.line, "##", 2)))
 		return (1);
 	valide_link(l, l->p.line);
-	//while (1);
 	while ((l->p.line[len_1] != '-'))
 		++len_1;
 	l->p.name_check = ft_strsub(l->p.line, 0, len_1);
